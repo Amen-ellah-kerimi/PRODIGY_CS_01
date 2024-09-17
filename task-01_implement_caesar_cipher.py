a69b5cdc010526abd53eaa52899ee9d7e7c71eaf
Amen-ellah-kerimi <mamap4110@gmail.com>
2024-09-17 10:14:30 +0100

    update Caesar Cipher implementation with refactored code

--- task-01_implement_caesar_cipher.py
+++ task-01_implement_caesar_cipher.py
@@ -1,10 +1,9 @@
 #task-01
 #Implement Caesar Cipher
-
+import string
+alphabet=list(string.ascii_lowercase) 
 #!functions
 def encryption (message,shift):
-    import string
-    alphabet=list(string.ascii_lowercase)
     encrypted_message = ""
     for char in message:
         if char in alphabet:
@@ -16,8 +15,6 @@ def encryption (message,shift):
     return encrypted_message
 
 def decryption(message,shift):
-    import string
-    alphabet=list(string.ascii_lowercase) 
     for char in message:
         if char in alphabet:
             position = alphabet.index(char)
@@ -30,12 +27,14 @@ def output (encrypted_message):
     print("the encrypted message is << " + encrypted_message + " >>")
 
 #?Main
-message=input("input the message : ")
-shift=int(input("input the shift value : "))
-a=int(input("1 for encryption or 2 for decryption"))
-if a==1:
-    new_message=encryption(message,shift)
-    print(new_message)
-elif a==2:
-    new_message=decryption(message,shift)
-    print(new_message)
+message=input("Enter the message : ")
+shift=int(input("Enter the shift value : "))
+action=int(input("Enter 1 to encrypt or 2 to decrypt :"))
+if action == 1:
+    result = encryption(message, shift)
+    print(f"Encrypted message: {result}")
+elif action == 2:
+    result = decryption(message, shift)
+    print(f"Decrypted message: {result}")
+else:
+    print("Invalid action. Please try again.")
